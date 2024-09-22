f6593d86d4aecda68bf37f3557c4f5ee23f38507
Danish Khan <khandanish78866@gmail.com>
2024-09-22 15:06:44 +0530

    Middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,19 @@
-import { authMiddleware } from '@kinde-oss/kinde-auth-nextjs/server'
+// import { authMiddleware } from '@kinde-oss/kinde-auth-nextjs/server'
 
-export const config = {
-  matcher: ['/dashboard/:path*', '/auth-callback'],
+// export const config = {
+//   matcher: ['/dashboard/:path*', '/auth-callback'],
+// }
+
+// export default authMiddleware
+
+// https://docs.kinde.com/developer-tools/sdks/backend/nextjs-prev-sdk/
+
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+export default function middleware(req: any) {
+  return withAuth(req, {
+    isReturnToCurrentPage: true
+  });
 }
-
-export default authMiddleware
+export const config = {
+  matcher: ['/dashboard/:path*', '/auth-callback']
+};
